1811fa5c9db351c4f72a57ef0d2f7171d5231d41
digriz <digriz@riseup.net>
2016-05-26 17:33:04 +0200

    refactor

--- source/detector.h
+++ source/detector.h
@@ -1,3 +1,16 @@
+#include "cv.h"
 
 
+class Detector
+{
+    public:
+        Detector();
+        virtual ~Detector();
+
+        cv::Mat trackObject(cv::Mat frame);
+        void detectObject();
+
+    private:
+
+};
 
--- source/logger.cpp
+++ source/logger.cpp
@@ -9,9 +9,12 @@ Logger::Logger()
     instance = NULL;
 }
 
+
 Logger::~Logger()
 {
+
 }
+
 
 Logger *Logger::getInstance()
 {
@@ -22,18 +25,22 @@ Logger *Logger::getInstance()
     return instance;
 }
 
+
 void Logger::error(std::string message)
 {
     std::cout << "error: " << message;
 }
+
 void Logger::warning(std::string message)
 {
     std::cout << "warning: " << message;
 }
+
 void Logger::debug(std::string message)
 {
     std::cout << "debug: " << message;
 }
+
 void Logger::debug(std::string message, int level)
 {
     std::cout << "debug[" << level << "]: " << message;
